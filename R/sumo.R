d2ab921242fa2b2c2a527b7f147a141188e5f149
Andrew Hooker <andrew.c.hooker@gmail.com>
2014-10-17 20:05:33 +0200

    New sumo function

---
+++ R/sumo.R
@@ -0,0 +1,17 @@
+sumo <- function(output_files,
+                 ssh_command = NULL, #"ssh andy@doris.biof.uu.se 'cd AOD/",
+                 run_dir=NULL,
+                 additional_commands=NULL,
+                 ...)
+{
+  
+  if(!is.null(ssh_command)) ssh_command <- paste(ssh_command,"; ",sep="")
+  cd_command <- NULL
+  if(!is.null(run_dir)) cd_command <- paste("cd ",run_dir,"; ",sep="") 
+  command <-paste(ssh_command, cd_command, "sumo ", paste(output_files,collapse=" ")," ",additional_commands,sep="")
+
+  if(Sys.which("sumo")=="") command <- paste("source ~/.bashrc;",command,sep="")
+      
+  system(command) 
+
+}
